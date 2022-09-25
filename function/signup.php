ebc8b4ddbe49cd472443c19528c006c1ba9e9beb
ruwanmx-developer <ruwanmx.developer@gmail.com>
2022-09-25 10:18:15 +0530

    employee signup function created

--- function/signup.php
+++ function/signup.php
@@ -7,36 +7,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
-// emp signup function
-if(array_key_exists("emp_signup",$_POST)){
-    $email = $_POST["email"];
-    $enc_password = $_POST["password"];
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $sql = "SELECT * FROM users WHERE email='$email'";
-    $result = $__conn->query($sql);
-    $data = "";
-    if ($result->num_rows > 0) {
-        $data = [ 'code' => 'code_1' ];
-    } else{
-        $sql = "INSERT INTO users VALUES (NULL, '$email', '$enc_password', '$fname', '$lname', 0, 2)";
-        if ($__conn->query($sql) === TRUE) {
-                $_SESSION['ses_email'] = $email;
-                $_SESSION['ses_full_name'] = $fname. " ".$lname;
-                $_SESSION['ses_first_name'] = $fname;
-                $_SESSION['ses_last_name'] = $lname;
-                $_SESSION['ses_role_id'] = '2';
-                $_SESSION['ses_user_state'] = 0;
-                $data = [ 'code' => 'code_3', 'role' => 2 ];
-        } else {
-            $data = [ 'code' => 'code_2' ];
-        }
-    }  
-    header('Content-type: application/json');
-    echo json_encode( $data );
-}
-
 // com signup function
 if(array_key_exists("com_signup",$_POST)){
     $email = $_POST["email"];
@@ -68,6 +38,48 @@ if(array_key_exists("com_signup",$_POST)){
                 $_SESSION['ses_role_id'] = 3;
                 $_SESSION['ses_user_state'] = 2;
                 $data = [ 'code' => 'code_3', 'role' => 3 ]; // success
+            } else {
+                $data = [ 'code' => 'code_2' ]; // unexpected
+            }
+        } else {
+            $data = [ 'code' => 'code_2' ]; // unexpected
+        }
+    }
+    header('Content-type: application/json');
+    echo json_encode( $data );
+}
+
+// emp signup function
+if(array_key_exists("emp_signup",$_POST)){
+    $email = $_POST["email"];
+    $enc_password = $_POST["password"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $logo = $_POST["image"];
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $__conn->query($sql);
+    $data = "";
+    if ($result->num_rows > 0) {
+        $data = [ 'code' => 'code_1' ]; // email exists
+    } else{
+        $sql2 = "INSERT INTO users VALUES (NULL, '$email', '$enc_password', 1, 2)";
+        if ( ($__conn->query($sql2) === TRUE)) {
+            $sql3 = "SELECT * FROM users WHERE email='$email'";
+            $result = $__conn->query($sql3);
+            $row = $result->fetch_assoc();
+            $image = "user_" . $row['user_id'] . ".png";
+            list($type, $logo) = explode(';', $logo);
+            list(, $logo) = explode(',', $logo);
+            $logo = base64_decode($logo);
+            file_put_contents('../uploads/user/' . $image, $logo);
+            $sql = "INSERT INTO employies VALUES ('".$row['user_id']."', '$fname', '$lname',NULL,'$image', NULL, NULL,NULL,NULL,NULL,NULL)";
+            if ( ($__conn->query($sql) === TRUE)) {
+                $_SESSION['ses_email'] = $email;
+                $_SESSION['ses_full_name'] = $fname . " " . $lname;
+                $_SESSION['ses_first_name'] = $fname;
+                $_SESSION['ses_role_id'] = 2;
+                $_SESSION['ses_user_state'] = 1;
+                $data = [ 'code' => 'code_3', 'role' => 2 ]; // success
             } else {
                 $data = [ 'code' => 'code_2' ]; // unexpected
             }
--- signing_e.php
+++ signing_e.php
@@ -50,6 +50,87 @@
                         </div>
                         <hr class="mt-3">
                         <div class="sep-link marg-b">Employee Details</div>
+                        <!-- image upload start -->
+                        <div class="col-12  marg-b d-flex justify-content-center">
+                            <label for="upload_image">
+                                <div class="form-control upload text-center">
+                                    <img class="upload-img" src="site_images/user.png" alt="">
+                                </div>
+                                <input type="file" name="upload_image" id="upload_image" class="d-none">
+                            </label>
+                        </div>
+                        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title crop-title">Crop Image Before Upload</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="img-container">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <img src="" id="sample_image" />
+                                                </div>
+                                                <div class="col-12 mt-3 d-flex justify-content-center">
+                                                    <div class="preview marg-r"></div>
+                                                    <div class="preview r marg-l"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" id="crop" class="btn btn-crop">Crop</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <script>
+                        var uploaded_image = "";
+                        $(document).ready(function() {
+                            var $modal = $('#modal');
+                            var image = document.getElementById('sample_image');
+                            var cropper;
+                            $('#upload_image').change(function(event) {
+                                var files = event.target.files;
+                                var done = function(url) {
+                                    image.src = url;
+                                    $modal.modal('show');
+                                };
+                                if (files && files.length > 0) {
+                                    reader = new FileReader();
+                                    reader.onload = function(event) {
+                                        done(reader.result);
+                                    };
+                                    reader.readAsDataURL(files[0]);
+                                }
+                            });
+                            $modal.on('shown.bs.modal', function() {
+                                cropper = new Cropper(image, {
+                                    aspectRatio: 1,
+                                    viewMode: 3,
+                                    dragMode: 'move',
+                                    preview: '.preview'
+                                });
+                            }).on('hidden.bs.modal', function() {
+                                cropper.destroy();
+                                cropper = null;
+                            });
+                            $('#crop').click(function() {
+                                canvas = cropper.getCroppedCanvas({
+                                    width: 200,
+                                    height: 200
+                                });
+                                var dataURL = canvas.toDataURL("image/png");
+                                console.log(dataURL);
+                                uploaded_image = dataURL;
+                                $('.upload-img').attr('src', dataURL);
+                                $modal.modal('hide');
+                            });
+
+                        });
+                        </script>
+                        <!-- image upload end -->
                         <div class="col-12 marg-b">
                             <input type="text" id="fname" class="form-control" placeholder="First Name">
                         </div>
@@ -120,12 +201,17 @@
             swal("Invalid field", fname + " is not a valid name. Enter a valid name", "error");
             return;
         }
+        if (uploaded_image === "") {
+            swal("Empty Field", "Please enter your profle image to continue", "warning");
+            return;
+        }
 
         let data = new FormData();
         data.append('email', email);
         data.append('fname', fname);
         data.append('lname', lname);
         data.append('password', enc_pass);
+        data.append('image', uploaded_image);
         data.append('emp_signup', 'true');
 
         var xhttp = new XMLHttpRequest();
@@ -151,7 +237,7 @@
                 }
             }
         };
-        xhttp.open("POST", "database/common/functions.php", true);
+        xhttp.open("POST", "./function/signup.php", true);
         xhttp.send(data);
     }
     </script>
