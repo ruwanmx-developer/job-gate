0ddeccfa0187b8b8a9b2eb2e0be199c85c8f8b29
ruwanmx-developer <ruwanmx.developer@gmail.com>
2022-11-15 01:07:36 +0530

    charts add and admin part 90% complete

--- components/header_links.php
+++ components/header_links.php
@@ -1,24 +1,40 @@
+<!-- meta data -->
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/x-icon" href="<?php echo $__siteroot; ?>./site_images/favicon.ico">
+
+<!-- bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
+
+<!-- bootstrap icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+<!-- sweet alert -->
 <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- cropper js -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.css"
     integrity="sha512-+VDbDxc9zesADd49pfvz7CgsOl2xREI/7gnzcdyA9XjuTxLXrdpuz21VVIqc5HPfZji2CypSbxx1lgD7BgBK5g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.js"
     integrity="sha512-ZK6m9vADamSl5fxBPtXw6ho6A4TuX89HUbcfvxa2v2NYNT/7l8yFGJ3JlXyMN4hlNbz0il4k6DvqbIW5CCwqkw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- charts js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<!-- javascripts -->
 <script src="<?php echo $__siteroot; ?>./js/validationPatterns.js"></script>
 <script src="<?php echo $__siteroot; ?>./js/md5.js"></script>
+
+<!-- css -->
 <link rel="stylesheet" href="<?php echo $__siteroot; ?>./css/main.css">
 <link rel="stylesheet" href="<?php echo $__siteroot; ?>./css/important.css">
 
--- user_admin/dashboard.php
+++ user_admin/dashboard.php
@@ -25,7 +25,98 @@
 
         <!-- middle bar -->
         <div class="col-lg-9 py-3 pe-3 ps-3 ps-lg-0">
+            <div class="title-bar card-basic py-2 mb-3">
+                SUMMARY
+            </div>
             <div class="row gx-3">
+                <div class="col-3">
+                    <div>
+                        <div class="title-bar-2 card-basic py-2">
+                            USER RATIO
+                            <canvas id="c1"></canvas>
+                        </div>
+                    </div>
+                    <script>
+                    const c1_data = {
+                        labels: [
+                            'Admins',
+                            'Companies',
+                            'Employies'
+                        ],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [
+                                <?php 
+                        $sql = "SELECT role_id, count(*) AS count FROM users GROUP BY role_id ORDER BY role_id";
+                        $result = $__conn->query($sql);
+                        while($row = $result->fetch_assoc()) {
+                            echo $row['count'] . ",";
+                        }
+                        ?>
+                            ],
+                            backgroundColor: [
+                                'rgb(255, 99, 132)',
+                                'rgb(54, 162, 235)',
+                                'rgb(255, 205, 86)'
+                            ],
+                            hoverOffset: 4
+                        }]
+                    };
+
+                    const c1_config = {
+                        type: 'doughnut',
+                        data: c1_data,
+                    };
+
+                    const c1 = new Chart(
+                        document.getElementById('c1'),
+                        c1_config
+                    );
+                    </script>
+                </div>
+                <div class="col-3">
+                    <div>
+                        <div class="title-bar-2 card-basic py-2">
+                            JOB COUNT
+                            <canvas id="c2"></canvas>
+                        </div>
+                    </div>
+                    <script>
+                    const c2_data = {
+                        labels: [
+                            'Active',
+                            'Closed',
+                        ],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [
+                                <?php 
+                        $sql = "SELECT count(*) AS count FROM jobs GROUP BY state ORDER BY state";
+                        $result = $__conn->query($sql);
+                        while($row = $result->fetch_assoc()) {
+                            echo $row['count'] . ",";
+                        }
+                        ?>
+                            ],
+                            backgroundColor: [
+                                'rgb(75, 192, 192)',
+                                'rgb(255, 99, 132)',
+                            ],
+                            hoverOffset: 4
+                        }]
+                    };
+
+                    const c2_config = {
+                        type: 'doughnut',
+                        data: c2_data,
+                    };
+
+                    const c2 = new Chart(
+                        document.getElementById('c2'),
+                        c2_config
+                    );
+                    </script>
+                </div>
 
             </div>
         </div>
