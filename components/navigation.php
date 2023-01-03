add235a24b046d5d182a4dfe21abeeec423f5811
ruwanmx-developer <ruwanmx.developer@gmail.com>
2023-01-03 22:28:10 +0530

    bug fixings

--- components/navigation.php
+++ components/navigation.php
@@ -7,46 +7,47 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto marg-b mb-lg-0">
-                <?php if(array_key_exists("ses_role_id", $_SESSION)){
-                if($_SESSION['ses_role_id'] === '2'){?>
+                <?php if (array_key_exists("ses_role_id", $_SESSION)) {
+                    if ($_SESSION['ses_role_id'] === '2') { ?>
                 <li class="nav-item">
-                    <a class="nav-link " href="<?php echo $__siteroot;?>./jobs.php">Jobs</a>
+                    <a class="nav-link " href="<?php echo $__siteroot; ?>./jobs.php">Jobssdsad</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $__siteroot;?>./companies.php">Companies</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                </li>
-                <?php } } else {?>
-                <li class="nav-item">
-                    <a class="nav-link " href="<?php echo $__siteroot;?>./jobs.php">Jobs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $__siteroot;?>./companies.php">Companies</a>
+                    <a class="nav-link" href="<?php echo $__siteroot; ?>./companies.php">Companies</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Services</a>
                 </li>
-                <?php }?>
+                <?php }
+                } else { ?>
+                <li class="nav-item">
+                    <a class="nav-link " href="<?php echo $__siteroot; ?>./jobs.php">Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $__siteroot; ?>./companies.php">Companies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <?php } ?>
             </ul>
             <div class="d-flex">
-                <?php if(array_key_exists('ses_email',$_SESSION)){?>
-                <?php 
-                $dashboard = "";
-                $user ="";
-                if($_SESSION['ses_role_id'] == 1){
-                    $dashboard = "$__siteroot./user_admin/dashboard.php";
-                    $user = "Administrator";
-                } else if($_SESSION['ses_role_id'] == 2){
-                    $dashboard = "$__siteroot./user_employee/dashboard.php";
-                    $user = $_SESSION['ses_full_name'];
-                } else if($_SESSION['ses_role_id'] == 3){
-                    $dashboard = "$__siteroot./user_company/dashboard.php";
-                    $user = $_SESSION['ses_company_name'];
-                }
-                ?>
-                <a href="<?php echo $dashboard; ?>" class="logout no-link"><?php echo $user;?></a>
+                <?php if (array_key_exists('ses_email', $_SESSION)) { ?>
+                <?php
+                    $dashboard = "";
+                    $user = "";
+                    if ($_SESSION['ses_role_id'] == 1) {
+                        $dashboard = "$__siteroot./user_admin/dashboard.php";
+                        $user = "Administrator";
+                    } else if ($_SESSION['ses_role_id'] == 2) {
+                        $dashboard = "$__siteroot./user_employee/dashboard.php";
+                        $user = $_SESSION['ses_full_name'];
+                    } else if ($_SESSION['ses_role_id'] == 3) {
+                        $dashboard = "$__siteroot./user_company/dashboard.php";
+                        $user = $_SESSION['ses_company_name'];
+                    }
+                    ?>
+                <a href="<?php echo $dashboard; ?>" class="logout no-link"><?php echo $user; ?></a>
                 <a href="<?php echo $__siteroot; ?>./function/authentication.php?logout=true" class="logout no-link">Log
                     Out</a>
                 <?php } else { ?>
--- user_company/view_applications.php
+++ user_company/view_applications.php
@@ -26,7 +26,7 @@
         <!-- middle bar -->
         <div class="col-lg-9 py-3 pe-3 ps-3 ps-lg-0">
             <div class="title-bar card-basic py-2 mb-3">
-                MANAGE CURRENT ACTIVE JOBS
+                VIEW APPLICATIONS
             </div>
             <div class="row gx-3">
                 <div class="col-12">
