4ca8d43cbe84e07a3cfb9a49b4207d8c432b1360
ruwanmx-developer <ruwanmx.developer@gmail.com>
2023-01-07 14:01:25 +0530

    bug fixings

--- database/database_config.php
+++ database/database_config.php
@@ -4,15 +4,13 @@ $__username = "root";
 $__password = "";
 $__dbname = "jobgatev2";
 $__page_error = "";
-$__site_url = "https://localhost/Websites/Mini%20Project%20V2/";
+$__site_url = "https://localhost/Websites/Job%20Gate/";
 
 try {
   $__conn = new mysqli($__servername, $__username, $__password, $__dbname);
+} catch (mysqli_sql_exception $__e) {
+  $_redtitle = 'Database Not Connected';
+  $_redmsg = 'This happend sometime when server is down or database is not operational.';
+  header("location:./redirect.php?title=$_redtitle&msg=$_redmsg");
+  return;
 }
-catch (mysqli_sql_exception $__e) {
-    $_redtitle = 'Database Not Connected'; 
-    $_redmsg = 'This happend sometime when server is down or database is not operational.'; 
-    header("location:./redirect.php?title=$_redtitle&msg=$_redmsg");
-    return;
-}
-?>
--- user_employee/components/employee_menu_card.php
+++ user_employee/components/employee_menu_card.php
@@ -42,30 +42,7 @@
             <div class=" ms-4 name active">PENDING APPLICATIONS</div>
         </div>
     </a>
-    <!-- <a href="view_accepted_applications.php" class="no-link ">
-        <div id="em_m_2_2" class="menu-item mb-2 ms-4 company-sec">
-            <div class="activebar <?php if ($admin_submenu == "em_m_2_2") {
-                                        echo "active";
-                                    } ?>"></div>
-            <div class=" ms-4 name active">ACCEPTED APPLICATIONS</div>
-        </div>
-    </a>
-    <a href="view_rejected_applications.php" class="no-link ">
-        <div id="em_m_2_3" class="menu-item mb-2 ms-4 company-sec">
-            <div class="activebar <?php if ($admin_submenu == "em_m_2_3") {
-                                        echo "active";
-                                    } ?>"></div>
-            <div class=" ms-4 name active">REJECTED APPLICATIONS</div>
-        </div>
-    </a> -->
-    <a href="" class="no-link ">
-        <div id="em_m_2_2" class="menu-item mb-2 ms-4 company-sec">
-            <div class="activebar <?php if ($admin_submenu == "em_m_2_2") {
-                                        echo "active";
-                                    } ?>"></div>
-            <div class=" ms-4 name active">INVITATIONS</div>
-        </div>
-    </a>
+
 </div>
 
 <div id="em_m_3" onclick="setMenu('em_m_3_wrap')" class="menu-item mb-2 company-prim">
@@ -114,30 +91,30 @@
 </div>
 
 <script>
-    const m1 = document.getElementById("em_m_1_wrap");
-    const m2 = document.getElementById("em_m_2_wrap");
-    const m3 = document.getElementById("em_m_3_wrap");
-    const m4 = document.getElementById("em_m_4_wrap");
+const m1 = document.getElementById("em_m_1_wrap");
+const m2 = document.getElementById("em_m_2_wrap");
+const m3 = document.getElementById("em_m_3_wrap");
+const m4 = document.getElementById("em_m_4_wrap");
 
-    if (!$(m1).is('.no-fade-jq')) {
-        $(m1).hide();
-    }
-    if (!$(m2).is('.no-fade-jq')) {
-        $(m2).hide();
-    }
-    if (!$(m3).is('.no-fade-jq')) {
-        $(m3).hide();
-    }
-    if (!$(m4).is('.no-fade-jq')) {
-        $(m4).hide();
-    }
+if (!$(m1).is('.no-fade-jq')) {
+    $(m1).hide();
+}
+if (!$(m2).is('.no-fade-jq')) {
+    $(m2).hide();
+}
+if (!$(m3).is('.no-fade-jq')) {
+    $(m3).hide();
+}
+if (!$(m4).is('.no-fade-jq')) {
+    $(m4).hide();
+}
 
-    function setMenu(x) {
-        let set = document.getElementById(x);
-        if (set.offsetParent === null) {
-            $(set).fadeIn();
-        } else {
-            $(set).fadeOut();
-        }
+function setMenu(x) {
+    let set = document.getElementById(x);
+    if (set.offsetParent === null) {
+        $(set).fadeIn();
+    } else {
+        $(set).fadeOut();
     }
+}
 </script>
